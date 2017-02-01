2024e4355c1f4490de05ac6ddb1daff7301c424a
AIZAWA Hina <hina@bouhime.com>
2017-02-01 19:04:28 +0900

    rawhideビルドをやめる
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -eu
 
 docker pull centos:7
 docker pull centos:6
-docker pull fedora:rawhide
+# docker pull fedora:rawhide
 
 for i in 7 6; do
   rm -rf centos${i}.build
@@ -16,11 +16,11 @@ for i in 7 6; do
   createrepo /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.0/el${i}/src/
 done
 
-rm -rf rawhide.build
-make rawhide
-find rawhide.build -type f -name '*.rpm' | xargs ./sign.exp
-mkdir -p /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.0/rawhide/x86_64/ /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.0/rawhide/src/
-cp -f rawhide.build/RPMS/x86_64/h2o-*.rpm /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.0/rawhide/x86_64/
-createrepo /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.0/rawhide/x86_64/
-cp -f rawhide.build/SRPMS/h2o-*.rpm /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.0/rawhide/src/
-createrepo /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.0/rawhide/src/
+# rm -rf rawhide.build
+# make rawhide
+# find rawhide.build -type f -name '*.rpm' | xargs ./sign.exp
+# mkdir -p /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.0/rawhide/x86_64/ /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.0/rawhide/src/
+# cp -f rawhide.build/RPMS/x86_64/h2o-*.rpm /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.0/rawhide/x86_64/
+# createrepo /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.0/rawhide/x86_64/
+# cp -f rawhide.build/SRPMS/h2o-*.rpm /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.0/rawhide/src/
+# createrepo /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.0/rawhide/src/
