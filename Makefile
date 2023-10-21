e1a59518feed37cb3025c43962b23a838dfc7aed
AIZAWA Hina <hina@fetus.jp>
2023-10-21 17:26:57 +0900

    b311c04

--- Makefile
+++ Makefile
@@ -1,38 +1,24 @@
-H2O_VERSION := 2.3.0-beta2
-LIBRESSL_VERSION := 3.4.3
+H2O_GIT_DATE := 20231011
+H2O_GIT_REF := b311c049d433a421e00bc52c442d47f373b949a1
+H2O_GIT_REF_SHORT := $(shell echo "${H2O_GIT_REF:0:7}")
 
-SOURCE_ARCHIVE := v$(H2O_VERSION).tar.gz
-LIBRESSL_ARCHIVE := libressl-$(LIBRESSL_VERSION).tar.gz
+SOURCE_ARCHIVE := h2o-$(H2O_GIT_REF).tar.gz
 TARGZ_FILE := h2o.tar.gz
 IMAGE_NAME := h2o-23-package
-centos6: IMAGE_NAME := $(IMAGE_NAME)-el6
 centos7: IMAGE_NAME := $(IMAGE_NAME)-el7
 centos8: IMAGE_NAME := $(IMAGE_NAME)-el8
-fedora: IMAGE_NAME := $(IMAGE_NAME)-fc23
-rawhide: IMAGE_NAME := $(IMAGE_NAME)-rawhide
-opensuse: IMAGE_NAME := $(IMAGE_NAME)-suse13.2
-amzn1: IMAGE_NAME := $(IMAGE_NAME)-amzn1
-amzn2: IMAGE_NAME := $(IMAGE_NAME)-amzn1
+centos8: IMAGE_NAME := $(IMAGE_NAME)-el9
 
-.PHONY: all clean centos6 centos7 fedora rawhide opensuse amzn1 amzn2
-
-all: centos6 centos7 centos8 fedora rawhide amzn1 amzn2
-centos6: centos6.build
+.PHONY: all centos7 centos8 centos9
+all: centos7 centos8 centos9
 centos7: centos7.build
 centos8: centos8.build
-fedora: fedora.build
-rawhide: rawhide.build
-opensuse: opensuse.build
-amzn1: amzn1.build
-amzn2: amzn2.build
+centos9: centos9.build
 
-rpmbuild/SOURCES/$(SOURCE_ARCHIVE):
-	curl -fSL https://github.com/h2o/h2o/archive/$(SOURCE_ARCHIVE) -o $@
+rpmbuild/SOURCES/h2o-$(H2O_GIT_REF).tar.gz:
+	curl -fsSL https://github.com/h2o/h2o/archive/$(H2O_GIT_REF).tar.gz -o $@
 
-rpmbuild/SOURCES/$(LIBRESSL_ARCHIVE):
-	curl -fSL https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-$(LIBRESSL_VERSION).tar.gz -o $@
-
-%.build: rpmbuild/SPECS/h2o.spec rpmbuild/SOURCES/$(SOURCE_ARCHIVE) rpmbuild/SOURCES/$(LIBRESSL_ARCHIVE)
+%.build: rpmbuild/SPECS/h2o.spec rpmbuild/SOURCES/$(SOURCE_ARCHIVE)
 	[ -d $@.bak ] && rm -rf $@.bak || :
 	[ -d $@ ] && mv $@ $@.bak || :
 	cp Dockerfile.$* Dockerfile
@@ -47,13 +33,9 @@ rpmbuild/SOURCES/$(LIBRESSL_ARCHIVE):
 	rm -rf tmp Dockerfile
 	docker images | grep -q $(IMAGE_NAME) && docker rmi $(IMAGE_NAME) || true
 
+.PHONY: clean
 clean:
 	rm -rf *.build.bak *.build tmp Dockerfile
-	docker images | grep -q $(IMAGE_NAME)-el6 && docker rmi $(IMAGE_NAME)-el6 || true
 	docker images | grep -q $(IMAGE_NAME)-el7 && docker rmi $(IMAGE_NAME)-el7 || true
 	docker images | grep -q $(IMAGE_NAME)-el8 && docker rmi $(IMAGE_NAME)-el8 || true
-	docker images | grep -q $(IMAGE_NAME)-fc23 && docker rmi $(IMAGE_NAME)-fc23 || true
-	docker images | grep -q $(IMAGE_NAME)-rawhide && docker rmi $(IMAGE_NAME)-rawhide || true
-	docker images | grep -q $(IMAGE_NAME)-suse13.2 && docker rmi $(IMAGE_NAME)-suse13.2 || true
-	docker images | grep -q $(IMAGE_NAME)-amzn1 && docker rmi $(IMAGE_NAME)-amzn1 || true
-	docker images | grep -q $(IMAGE_NAME)-amzn2 && docker rmi $(IMAGE_NAME)-amzn2 || true
+	docker images | grep -q $(IMAGE_NAME)-el9 && docker rmi $(IMAGE_NAME)-el9 || true
--- build.sh
+++ build.sh
@@ -2,17 +2,17 @@
 
 set -eu
 
-for i in 8; do
+for i in 9 8; do
   docker pull rockylinux:$i
   rm -rf centos${i}.build
   make centos${i}
   find centos${i}.build -type f -name '*.rpm' | xargs rpmsign --resign --key-id=C9F367D2
-  mkdir -p /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/el${i}/x86_64/
-  mkdir -p /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/el${i}/src/
-  cp -f centos${i}.build/RPMS/x86_64/h2o-*.rpm /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/el${i}/x86_64/
-  createrepo /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/el${i}/x86_64/
-  cp -f centos${i}.build/SRPMS/h2o-*.rpm /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/el${i}/src/
-  createrepo /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/el${i}/src/
+  # mkdir -p /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/el${i}/x86_64/
+  # mkdir -p /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/el${i}/src/
+  # cp -f centos${i}.build/RPMS/x86_64/h2o-*.rpm /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/el${i}/x86_64/
+  # createrepo /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/el${i}/x86_64/
+  # cp -f centos${i}.build/SRPMS/h2o-*.rpm /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/el${i}/src/
+  # createrepo /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/el${i}/src/
 done
 
 for i in 7; do
@@ -20,33 +20,10 @@ for i in 7; do
   rm -rf centos${i}.build
   make centos${i}
   find centos${i}.build -type f -name '*.rpm' | xargs rpmsign --resign --key-id=C9F367D2
-  mkdir -p /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/el${i}/x86_64/
-  mkdir -p /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/el${i}/src/
-  cp -f centos${i}.build/RPMS/x86_64/h2o-*.rpm /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/el${i}/x86_64/
-  createrepo /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/el${i}/x86_64/
-  cp -f centos${i}.build/SRPMS/h2o-*.rpm /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/el${i}/src/
-  createrepo /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/el${i}/src/
+  # mkdir -p /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/el${i}/x86_64/
+  # mkdir -p /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/el${i}/src/
+  # cp -f centos${i}.build/RPMS/x86_64/h2o-*.rpm /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/el${i}/x86_64/
+  # createrepo /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/el${i}/x86_64/
+  # cp -f centos${i}.build/SRPMS/h2o-*.rpm /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/el${i}/src/
+  # createrepo /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/el${i}/src/
 done
-
-# docker pull fedora:rawhide
-# rm -rf rawhide.build
-# make rawhide
-# find rawhide.build -type f -name '*.rpm' | xargs ./sign.exp
-# mkdir -p /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/rawhide/x86_64/ /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/rawhide/src/
-# cp -f rawhide.build/RPMS/x86_64/h2o-*.rpm /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/rawhide/x86_64/
-# createrepo /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/rawhide/x86_64/
-# cp -f rawhide.build/SRPMS/h2o-*.rpm /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/rawhide/src/
-# createrepo /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/rawhide/src/
-
-# for i in 1 2; do
-#   docker pull amazonlinux:$i
-#   rm -rf amzn${i}.build
-#   make amzn${i}
-#   find amzn${i}.build -type f -name '*.rpm' | xargs ./sign.exp
-#   mkdir -p /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/amzn${i}/x86_64/
-#   mkdir -p /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/amzn${i}/src/
-#   cp -f amzn${i}.build/RPMS/x86_64/h2o-*.rpm /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/amzn${i}/x86_64/
-#   createrepo /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/amzn${i}/x86_64/
-#   cp -f amzn${i}.build/SRPMS/h2o-*.rpm /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/amzn${i}/src/
-#   createrepo /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/amzn${i}/src/
-# done
