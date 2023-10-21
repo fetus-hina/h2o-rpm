fdcb903a81c4fbd8a2327299c2978f4d5b137095
AIZAWA Hina <hina@fetus.jp>
2023-10-21 18:01:57 +0900

    Enable deploy

--- build.sh
+++ build.sh
@@ -7,12 +7,12 @@ for i in 9 8; do
   rm -rf centos${i}.build
   make centos${i}
   find centos${i}.build -type f -name '*.rpm' | xargs rpmsign --resign --key-id=C9F367D2
-  # mkdir -p /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/el${i}/x86_64/
-  # mkdir -p /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/el${i}/src/
-  # cp -f centos${i}.build/RPMS/x86_64/h2o-*.rpm /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/el${i}/x86_64/
-  # createrepo /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/el${i}/x86_64/
-  # cp -f centos${i}.build/SRPMS/h2o-*.rpm /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/el${i}/src/
-  # createrepo /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/el${i}/src/
+  mkdir -p /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-rolling/el${i}/x86_64/
+  mkdir -p /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-rolling/el${i}/src/
+  cp -f centos${i}.build/RPMS/x86_64/h2o-*.rpm /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-rolling/el${i}/x86_64/
+  createrepo /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-rolling/el${i}/x86_64/
+  cp -f centos${i}.build/SRPMS/h2o-*.rpm /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-rolling/el${i}/src/
+  createrepo /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-rolling/el${i}/src/
 done
 
 for i in 7; do
@@ -20,10 +20,10 @@ for i in 7; do
   rm -rf centos${i}.build
   make centos${i}
   find centos${i}.build -type f -name '*.rpm' | xargs rpmsign --resign --key-id=C9F367D2
-  # mkdir -p /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/el${i}/x86_64/
-  # mkdir -p /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/el${i}/src/
-  # cp -f centos${i}.build/RPMS/x86_64/h2o-*.rpm /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/el${i}/x86_64/
-  # createrepo /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/el${i}/x86_64/
-  # cp -f centos${i}.build/SRPMS/h2o-*.rpm /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/el${i}/src/
-  # createrepo /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-2.3/el${i}/src/
+  mkdir -p /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-rolling/el${i}/x86_64/
+  mkdir -p /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-rolling/el${i}/src/
+  cp -f centos${i}.build/RPMS/x86_64/h2o-*.rpm /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-rolling/el${i}/x86_64/
+  createrepo /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-rolling/el${i}/x86_64/
+  cp -f centos${i}.build/SRPMS/h2o-*.rpm /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-rolling/el${i}/src/
+  createrepo /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-rolling/el${i}/src/
 done
