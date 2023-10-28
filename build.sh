f97de9e1c2cf465cb3e1d9f99aa54b677a94c599
AIZAWA Hina <hina@fetus.jp>
2023-10-28 17:49:19 +0900

    2.3.0-1.git20231027.0.e42f8ef

--- build.sh
+++ build.sh
@@ -15,6 +15,8 @@ for i in 9 8 7; do
   find centos${i}.build -type f -name '*.rpm' | xargs rpmsign --resign --key-id=C9F367D2
 
   if [ ${DEPLOY:-0} -ne 0 ]; then
+    echo "Deploying el${i}..."
+
     mkdir -p /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-rolling/el${i}/x86_64/
     mkdir -p /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-rolling/el${i}/src/
     cp -f centos${i}.build/RPMS/x86_64/h2o-*.rpm /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/h2o-rolling/el${i}/x86_64/
